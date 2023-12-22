8bde75ba8a41bebd0858fdf1dc1815651fe12238
NatalieShik <144128581+NatalieShik@users.noreply.github.com>
2023-12-22 16:36:17 +0300

    Файлы библиотеки из 6 лабораторной

---
+++ ClassLibraryFor4567/Class4567.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibraryFor4567
+{
+    public class Class4567
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?%!№ ;*()_-=+";
+        static Random random = new Random();
+        public string GenerateCaptcha(int num)
+        {
+            return new string(Enumerable.Repeat(chars, num).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
